a496977151d8edd9956e1f3b9859453f87018cad
tahani-cherif <cheriftahani92@gmail.com>
2024-05-12 13:32:27 +0100

    stape 2

--- src/pages/projet/ProjetById.js
+++ src/pages/projet/ProjetById.js
@@ -99,157 +99,170 @@ function ProjetById() {
   };
 
   return (
-    <MainCard>
-      <div style={{ marginBottom: '20px', display: 'flex', flexDirection: 'column' }}>
-        <Steps
-          type="navigation"
-          size="small"
-          current={current}
-          onChange={onChange}
-          className="site-navigation-steps"
-          items={[
-            {
-              title: 'Importer'
-            },
-            {
-              title: 'Préparer'
-            },
-            {
-              title: 'Placer'
-            },
-            {
-              title: 'Couper'
-            }
-          ]}
-        />
-      </div>
-      {current === 0 && (
-        <ImporterFiles
-          handleUpdateProjectName={handleUpdateProjectName}
-          handleFileSelect={handleFileSelect}
-          handleUploadedFiles={handleUploadedFiles}
-        />
-      )}
-      {current === 1 && (
-        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-          <div style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gap: '10px', borderBottom: '1px solid gray' }}>
-            <p style={{ fontSize: '18px', fontWeight: '700' }}>Image</p>
-            <p style={{ fontSize: '18px', fontWeight: '700' }}>Quantité</p>
-            <p style={{ fontSize: '18px', fontWeight: '700' }}>Largeur</p>
-            <p style={{ fontSize: '18px', fontWeight: '700' }}>Hauteur</p>
-            <p style={{ fontSize: '18px', fontWeight: '700' }}>Priorité</p>
-          </div>
-          {files?.map((item, index) => {
-            return (
-              <div
-                key={index}
-                style={{
-                  display: 'grid',
-                  gridTemplateColumns: 'repeat(5, 1fr)',
-                  gap: '10px',
-                  alignItems: 'center',
-                  borderBottom: '1px solid gray'
-                }}
-              >
-                <img src={item.path} alt={`image` + index} style={{ width: '200px', height: '150px' }} />
-                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
-                  <button
-                    onClick={() => handleQuantityChange(item.id, Math.max(1, item.quantity - 1))}
-                    style={{
-                      cursor: 'pointer',
-                      background: 'rgb(18, 204, 4)',
-                      color: 'white',
-                      border: '0',
-                      fontSize: '18px',
-                      borderRadius: '5px',
-                      width: '25px',
-                      height: '25px'
-                    }}
-                  >
-                    -
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button
-                    onClick={() => handleQuantityChange(item.id, item.quantity + 1)}
-                    style={{
-                      cursor: 'pointer',
-                      background: 'rgb(18, 204, 4)',
-                      color: 'white',
-                      border: '0',
-                      fontSize: '18px',
-                      borderRadius: '5px',
-                      width: '25px',
-                      height: '25px'
-                    }}
-                  >
-                    +
-                  </button>
-                </div>
-                <div>{item.width && item.width.toFixed(2)}</div>
-                <div>{item.height && item.height.toFixed(2)}</div>
-                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
-                  <button
-                    onClick={() => handlePriorityChange(item.id, Math.max(0, item.priority - 1))}
-                    style={{
-                      cursor: 'pointer',
-                      background: 'rgb(18, 204, 4)',
-                      color: 'white',
-                      border: '0',
-                      fontSize: '18px',
-                      borderRadius: '5px',
-                      width: '25px',
-                      height: '25px'
-                    }}
-                  >
-                    -
-                  </button>
-                  <span>{item.priority}</span>
-                  <button
-                    onClick={() => handlePriorityChange(item.id, item.priority + 1)}
-                    style={{
-                      cursor: 'pointer',
-                      background: 'rgb(18, 204, 4)',
-                      color: 'white',
-                      border: '0',
-                      fontSize: '18px',
-                      borderRadius: '5px',
-                      width: '25px',
-                      height: '25px'
-                    }}
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
-            );
-          })}
+    <div>
+      <MainCard>
+        <div style={{ marginBottom: '20px', display: 'flex', flexDirection: 'column' }}>
+          <Steps
+            type="navigation"
+            size="small"
+            current={current}
+            onChange={onChange}
+            className="site-navigation-steps"
+            items={[
+              {
+                title: 'Importer'
+              },
+              {
+                title: 'Préparer'
+              },
+              {
+                title: 'Placer'
+              },
+              {
+                title: 'Couper'
+              }
+            ]}
+          />
         </div>
-      )}
-
-      {current === 2 && <Placer />}
-      {current === 3 && <Couper />}
-
-      <div style={{ marginTop: '50px', display: 'flex', justifyContent: 'space-between' }}>
-        {current === 0 ? (
-          <Button startIcon={<ArrowBackIcon />} style={{ marginRight: '8px', width: '50%' }} onClick={() => navigate('/projet')}>
-            List de projet
-          </Button>
-        ) : (
-          <Button style={{ marginRight: '8px', width: '50%' }} onClick={handlePrev}>
-            Précédent
-          </Button>
+        {current === 0 && (
+          <ImporterFiles
+            handleUpdateProjectName={handleUpdateProjectName}
+            handleFileSelect={handleFileSelect}
+            handleUploadedFiles={handleUploadedFiles}
+          />
+        )}
+        {current === 1 && (
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gap: '10px', borderBottom: '1px solid gray' }}>
+              <p style={{ fontSize: '18px', fontWeight: '700' }}>Image</p>
+              <p style={{ fontSize: '18px', fontWeight: '700' }}>Quantité</p>
+              <p style={{ fontSize: '18px', fontWeight: '700' }}>Largeur</p>
+              <p style={{ fontSize: '18px', fontWeight: '700' }}>Hauteur</p>
+              <p style={{ fontSize: '18px', fontWeight: '700' }}>Priorité</p>
+            </div>
+            {files?.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(5, 1fr)',
+                    gap: '10px',
+                    alignItems: 'center',
+                    borderBottom: '1px solid gray'
+                  }}
+                >
+                  <img src={item.path} alt={`image` + index} style={{ width: '200px', height: '150px' }} />
+                  <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
+                    <button
+                      onClick={() => handleQuantityChange(item.id, Math.max(1, item.quantity - 1))}
+                      style={{
+                        cursor: 'pointer',
+                        background: 'rgb(18, 204, 4)',
+                        color: 'white',
+                        border: '0',
+                        fontSize: '18px',
+                        borderRadius: '5px',
+                        width: '25px',
+                        height: '25px'
+                      }}
+                    >
+                      -
+                    </button>
+                    <span>{item.quantity}</span>
+                    <button
+                      onClick={() => handleQuantityChange(item.id, item.quantity + 1)}
+                      style={{
+                        cursor: 'pointer',
+                        background: 'rgb(18, 204, 4)',
+                        color: 'white',
+                        border: '0',
+                        fontSize: '18px',
+                        borderRadius: '5px',
+                        width: '25px',
+                        height: '25px'
+                      }}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div>{item.width && item.width.toFixed(2)}</div>
+                  <div>{item.height && item.height.toFixed(2)}</div>
+                  <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
+                    <button
+                      onClick={() => handlePriorityChange(item.id, Math.max(0, item.priority - 1))}
+                      style={{
+                        cursor: 'pointer',
+                        background: 'rgb(18, 204, 4)',
+                        color: 'white',
+                        border: '0',
+                        fontSize: '18px',
+                        borderRadius: '5px',
+                        width: '25px',
+                        height: '25px'
+                      }}
+                    >
+                      -
+                    </button>
+                    <span>{item.priority}</span>
+                    <button
+                      onClick={() => handlePriorityChange(item.id, item.priority + 1)}
+                      style={{
+                        cursor: 'pointer',
+                        background: 'rgb(18, 204, 4)',
+                        color: 'white',
+                        border: '0',
+                        fontSize: '18px',
+                        borderRadius: '5px',
+                        width: '25px',
+                        height: '25px'
+                      }}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         )}
 
-        <Button
-          style={{ width: '50%' }}
-          variant="contained"
-          onClick={handleNext}
-          disabled={current === totalSteps - 1 || (current === 0 && uploadedFilesCount === 0 && selectedProject?.fileCount === 0)}
-        >
-          Suivant
-        </Button>
+        {current === 2 && <Placer handleQuantityChange={handleQuantityChange} handlePriorityChange={handlePriorityChange} files={files} />}
+        {current === 3 && <Couper />}
+
+        <div style={{ marginTop: '50px', display: 'flex', justifyContent: 'space-between' }}>
+          {current === 0 ? (
+            <Button startIcon={<ArrowBackIcon />} style={{ marginRight: '8px', width: '50%' }} onClick={() => navigate('/projet')}>
+              List de projet
+            </Button>
+          ) : (
+            <Button style={{ marginRight: '8px', width: '50%' }} onClick={handlePrev}>
+              Précédent
+            </Button>
+          )}
+
+          <Button
+            style={{ width: '50%' }}
+            variant="contained"
+            onClick={handleNext}
+            disabled={current === totalSteps - 1 || (current === 0 && uploadedFilesCount === 0 && selectedProject?.fileCount === 0)}
+          >
+            Suivant
+          </Button>
+        </div>
+      </MainCard>
+      <div style={{ display: 'flex' }}>
+        <button style={{ width: '58.3333%' }}>eeeee</button>
+        <div
+          style={{
+            height: '100%',
+            width: '8%',
+            background: 'repeating-linear-gradient(-75deg, rgb(126, 211, 33), rgb(126, 211, 33) 49%, rgb(74, 74, 74) 51%, rgb(74, 74, 74))'
+          }}
+        ></div>
+        <button style={{ width: '', background: 'rgb(126, 211, 33)' }}>eeeee</button>
       </div>
-    </MainCard>
+    </div>
   );
 }
 
